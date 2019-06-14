25a6c4fa5c3732b1a614b3bdd97e428bec0f54f6
Carlos <1310374@utp.edu.pe>
2019-06-13 20:25:59 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from "@angular/common";
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
+
+import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContraseniaPipe } from './pipes/contrasenia.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContraseniaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
